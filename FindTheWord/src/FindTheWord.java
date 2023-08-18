e0d1cc42d0f265e6d8528f5f5aa91811ae38cc21
SKPatil18 <85570532+SKPatil18@users.noreply.github.com>
2023-08-18 22:44:43 +0530

    Add files via upload

---
+++ FindTheWord/src/FindTheWord.java
@@ -0,0 +1,9 @@
+
+public class FindTheWord {
+	public static String find(String str,int n) {
+		String words[] = str.split(" ");
+		return words[n] ;
+		
+	}
+
+}
---
+++ FindTheWord/src/FindTheWordApp.java
@@ -0,0 +1,12 @@
+
+public class FindTheWordApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I love Codecademy";
+		int n = 2;
+		System.out.println(FindTheWord.find(str,n));
+
+	}
+
+}
